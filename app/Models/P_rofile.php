b25c8cb9b0e917c5c1984ea7872ed3cdd1f78b00
swidodo <smtwidodo04@gmail.com>
2024-05-07 22:40:24 +0700

    first

--- app/Models/P_rofile.php
+++ app/Models/P_rofile.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class profile extends Model
+class P_rofile extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
