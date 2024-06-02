40954dc373cfc2ca506fd5db63a70d882b6c0eb3
swidodo <smtwidodo04@gmail.com>
2024-06-02 20:09:21 +0700

    update

---
+++ app/Http/Controllers/HubungiController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HubungiController extends Controller
+{
+    public function index()
+    {
+        $data['page']     = 'Kelas Rewata Consultant';
+        $data['title']    = 'Kelas Rewata consultant';
+        $data['kelas']    = Kelas::orderBy('id','DESC')->get();   
+        return view('kelas.index',$data);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
